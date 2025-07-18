a68fa407bca855cdd18873a0206f7d0c3f2e8d39
Ikaterin-ctrl <catarinaneedsajob@gmail.com>
2025-07-18 00:08:01 -0300

    fix paths for relative navigation

--- __tests__/caixa.test.js
+++ __tests__/caixa.test.js
@@ -1,5 +1,19 @@
 const { salvarLembranca } = require('../js/caixa');
 
+// Simples mock do localStorage para ambiente de testes
+global.localStorage = {
+  store: {},
+  getItem(key) {
+    return this.store[key] || null;
+  },
+  setItem(key, value) {
+    this.store[key] = String(value);
+  },
+  clear() {
+    this.store = {};
+  }
+};
+
 describe('salvarLembranca', () => {
   beforeEach(() => {
     localStorage.clear();
